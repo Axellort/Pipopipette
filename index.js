1ccf970ee23e79d7ed9a7cc7ba76319979bd19d9
Axellort <chone.axel@gmail.com>
2022-09-20 18:10:50 +0200

    fin du cours

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const COLORS = ["red", "blue"]
 const n = 2;
+console.log(document.getElementById(`border-0-0-V`))
 // récupérer les cases
 // --------------
-let bordersV = [0,0];
-let bordersH = [0,0];
+let bordersV = [[0,0],[0,0]];
+let bordersH = [[0,0],[0,0]];
 let tiles = [0,0];
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
         bordersV[i][j] = document.getElementById(`border-${i}-${j}-V`);
         bordersH[i][j] = document.getElementById(`border-${i}-${j}-H`);
         tiles[i][j] = { el: document.getElementById(`tile-${i}-${j}`), clicsH: [0, 0], clicsV: [0, 0], appartenance:-1 };
-        console.log(bordersH, bordersV, tiles)
+        console.log(bordersH[i][j], document.getElementById(`border-${i}-${j}-V`), tiles)
         bordersV[i][j].onclick = i == 0 ? () => { tiles[i][j].clicsV[1] = 1; checkRemplis(tiles, getNextJoueur()); } : () => {
             tiles[i - 1][j].clicsV[1] = 1;
             tiles[i][j].clicsV[1] = 1;
