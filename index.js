5cde4097865e87fca072a7e248e9c4235c202c4e
Ad4mo5 <adam.bouziane.ml@gmail.com>
2022-10-04 16:07:52 +0000

    pushcommentaire

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-
+//grille à n cotes
 const n = 5;
+
+//création de la grille
 createDom(n);
-//! SETTING CONSTANTS 
+
+
+// constantes couleur joueur
 const COLORS = ["red", "blue"]
 const CLASS_NAMES_PER_PLAYER = ["first", "second"];
 const CLASS_TAKEN = ["taken"];
 
-//! GETTING ELEMENTS AND SETTING ONCLICKS
-const scoresElementsIds = ["first-score", "second-score"];
-const scoresElements = scoresElementsIds.map((id) => document.getElementById(id))
+// recupere les elements fixes et onclick
 
-let bordersV = []; // TODO A MODIFIER, C'EST MOCHE COMME CA
+const scoresElementsIds = ["first-score", "second-score"];
+// pour chaque element de la lister du dessus je recupere l'element qui a cette id
+const scoresElements = scoresElementsIds.map((id) => document.getElementById(id))
+let bordersV = []; 
 let bordersH = [];
 let tiles = [];
 let playerActu = 0;
 for (let i = 0; i < n; i++) {
+// je crée 3 tableaux de n listes pour les cases et les bords horizontaux et verticaux    
     tiles.push([]);
     bordersH.push([]);
     bordersV.push([]);
     for (let j = 0; j < n; j++) {
 
         console.log(i + ", " + j);
-
+// dans chaque liste du tableau "tiles" j'ajoute l'element d'identifiant tile-coordonnéex-coordonéey 
         tiles[i].push({ el: document.getElementById(`tile-${i}-${j}`), clicsH: [0, 0], clicsV: [0, 0], appartenance: -1 });
+// de même pour les bords verticaux et horizontaux j'ajoute les elements border-coordonnéex-coordonnéey
         bordersV[i].push(document.getElementById(`border-${i}-${j}-V`));
         bordersH[i].push(document.getElementById(`border-${i}-${j}-H`));
+// je donne une fonction à chaque bord horizontal et vertical qui colore les bords selon la couleur du joueur(voirsetonclick)
         setOnClick(bordersH[i][j], "h", i, j);
         setOnClick(bordersV[i][j], "v", i, j);
 
     }
 }
-
+//je pose une variable right
 let right = n;
+// j'ajoute une liste à mon tableau pour l'extrêmité droite
 bordersV.push([]);
 for (let j = 0; j < n; j++) {
+//j'ajoute les éléments border-right-y pour passer de n à n+1 colonnes à ma grille
     bordersV[right].push(document.getElementById(`border-${right}-${j}-V`));
+//je donne à mes bords la fonction setonclick
     setOnClick(bordersV[right][j], "v", right, j);
 }
+//je pose une variable bottom
 let bottom = n;
 for (let i = 0; i < n; i++) {
+//dans chaque liste du tableau je rajoute l'élément border-x-bottom pour passer de n à n+1 lignes
     bordersH[i].push(document.getElementById(`border-${i}-${bottom}-H`));
+//je donne à mes bords setonclick
     setOnClick(bordersH[i][bottom], "h", i, bottom);
 }
 
+//
+
+//je crée ma fonction setonclick
 function setOnClick(el, str, i, j) {
+// si c'est un bord vertical
     if (str == "v") {
+//si je clique sur un élément je lance ce qu'il y a après
         el.onclick = () => {
+//si la case n'est pas une case sur les extrêmités
             if (i != n && j != n) { tiles[i][j].clicsV[0] = 1; }
             if (i != 0) { tiles[i - 1][j].clicsV[1] = 1; }
             remplir(bordersV[i][j]);
