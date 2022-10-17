78099b230804e39b42ab4ce78f3230cda7d71faf
Axellort <chone.axel@gmail.com>
2022-10-17 23:34:29 +0200

    start ( not working, still needs a lot of work)

--- server.js
+++ server.js
@@ -12,9 +12,17 @@ let playerActu = 0;
 let playerGiven = 0;
 let nJoueurs = 3;
 io.on('connection', (socket) => {
+    console.log("Connection");
+    let idOfSocket = '';
+    socket.on('game-id', (id) => {
+        idOfSocket = id;
+        socket.join(id);
+    })
+    // ! HOW TO : je récupère l'id de la partie au début et met mon socket dans la room. Quand je reçois un event de ce socket je l'émets avec io.of(id).emit (?) si id !==''
+    // TODO : comment gérer les ids ? + voir vidéo de WebDevSimplified
+    // TODO : when connection, if number of players for an id is full, give to each socket a playerID et mettre tous les événements dans les bonnes rooms
     const playerForSocket = playerGiven % nJoueurs;
     playerGiven++;
-    console.log("Connection");
 
     socket.emit("player-id", playerForSocket);
     if (playerGiven == nJoueurs) {
