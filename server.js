6c0729adadbdce0fb0b00442c620d0dd4a20c30b
Axellort <chone.axel@gmail.com>
2022-10-18 18:10:12 +0200

    not working 18/10

--- server.js
+++ server.js
@@ -10,47 +10,51 @@ app.use(express.static("public"));
 const n = 5;
 let playerActu = 0;
 let nJoueurs = 3;
-let playerPerRoom = {"default":0};
-io.on('connection', (socket) => {
-    console.log("Connection");
+let playerPerRoom = { default: 0 };
+io.on("connection", (socket) => {
+  console.log("Connection");
 
-    let playerForSocket = playerPerRoom[id] % nJoueurs;
-    playerPerRoom[id]++;
-    let idOfSocket = 'default';
+  let idOfSocket = "default";
 
-    socket.on('game-id', (id) => {
-        idOfSocket = id;
-        socket.join(id);
-        manageGame(id, socket, io);
-    })
-    // ! HOW TO : je récupère l'id de la partie au début et met mon socket dans la room. Quand je reçois un event de ce socket je l'émets avec io.of(id).emit (?) si id !==''
-    // TODO : comment gérer les ids ? + voir vidéo de WebDevSimplified
-    // TODO : when connection, if number of players for an id is full, give to each socket a playerID et mettre tous les événements dans les bonnes rooms
+  socket.on("game-id", (id) => {
+    idOfSocket = id;
+    socket.join(id);
+    manageGame(idOfSocket, socket, io);
+  });
+  // ! HOW TO : je récupère l'id de la partie au début et met mon socket dans la room. Quand je reçois un event de ce socket je l'émets avec io.of(id).emit (?) si id !==''
+  // TODO : comment gérer les ids ? + voir vidéo de WebDevSimplified
+  // TODO : when connection, if number of players for an id is full, give to each socket a playerID et mettre tous les événements dans les bonnes rooms
 
-    manageGame(id, socket,io);
-    
-})
+  socket.join(idOfSocket);
+  manageGame(idOfSocket, socket, io);
+});
 
+server.listen(3000, () => {
+  console.log(`🕸️ Listening on 3000`);
+});
 
+function manageGame(id, socket, io) {
+  console.log(`manageGame with id : ${id}`);
+  console.log(playerPerRoom[id]);
+  console.log(socket.rooms);
+  playerForSocket = playerPerRoom[id] ?? 0;
+  playerPerRoom[id] = playerForSocket + 1;
+  socket.emit("player-id", playerForSocket);
 
-server.listen(3000, () => { console.log(`🕸️ Listening on 3000`) })
-
-
-function manageGame(id, socket, io){
-    playerForSocket = playerPerRoom[id] % nJoueurs ?? 0;
-        playerPerRoom[id] = playerForSocket+1;
-        socket.emit("player-id", playerForSocket);
-
-        if (playerForSocket == nJoueurs) {
-            io.of(id).emit('new-game', n, nJoueurs);
-        }
-        // new game
-        socket.on('click', (player, i, j, str) => {
-            console.log("CLICK")
-            console.log(player)
-            io.of(id).emit("click", player, i, j, str);
-            playerActu++;
-            playerActu %= nJoueurs;
-        })
-        socket.on("disconnect", () => { console.log("deconnection") })
+  if (playerForSocket == nJoueurs) {
+    io.sockets.in(id).emit("new-game", n, nJoueurs);
+  }
+  // new game
+  socket.on("click", (player, i, j, str) => {
+    console.log("CLICK");
+    console.log(player);
+    console.log(socket.rooms);
+    io.sockets.in(id).emit("click", player, i, j, str);
+    playerActu++;
+    playerActu %= nJoueurs;
+  });
+  socket.on("disconnect", () => {
+    console.log("deconnection");
+    console.log(`Disconnected from id : ${id}`);
+  });
 }
