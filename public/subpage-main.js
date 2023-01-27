6bd4e7d8483dd75a40680bc12e922586c1a0bfc9
Adam Bouziane <adam.bouziane.ml@gmail.com>
2023-01-27 21:22:37 +0100

    subpage final version

---
+++ public/subpage-main.js
@@ -0,0 +1,11 @@
+
+function getInfoConnexion(){
+    let info = [document.getElementById("mail").value,
+               document.getElementById("mdp").value];
+}
+
+function getInfoInscription(){
+    let info = [document.getElementById("mail-inscri").value,
+    document.getElementById("mdp-inscri").value];
+
+}
